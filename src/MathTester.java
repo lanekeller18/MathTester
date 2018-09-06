3a08ac968744ad0d959d3c2eb1f88fe2f6f18614
lanekeller18 <42771134+lanekeller18@users.noreply.github.com>
2018-09-06 08:34:52 -0400

    Changed a variable and unblocked the entire code.

--- src/MathTester.java
+++ src/MathTester.java
@@ -10,17 +10,17 @@ public class MathTester {
         firstWord = "dog";
         secondWord = "pen";
         firstNumber = 30;
-        secondNumber = 10;
+        secondNumber = 90;
         total = firstNumber+secondNumber;
 
         System.out.println(total);
 
-        /*.out.println(firstWord+secondWord);
+        .out.println(firstWord+secondWord);
         System.out.println("the first number plus the second number is "+ (firstNumber+secondNumber));
         System.out.println("the second number minus the first number is "+ (secondNumber-firstNumber));
         System.out.println("the second number divided the first number is "+ (secondNumber/firstNumber));
         System.out.println("the first number times the second number is "+ (firstNumber*secondNumber));
-        System.out.println("the modulus of the second number by the first number is "+ (secondNumber%firstNumber));*/
+        System.out.println("the modulus of the second number by the first number is "+ (secondNumber%firstNumber));
 
 
     }
