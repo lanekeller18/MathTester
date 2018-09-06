684fc3bd3dd2e66fdb216f7d7ecd27f2ad6f139c
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-09-06 08:10:07 -0400

    Created the Mathtester class and preformed math.

---
+++ src/MathTester.java
@@ -0,0 +1,28 @@
+public class MathTester {
+
+    public static void main(String[]  args){
+
+        String firstWord;
+        String secondWord;
+        int firstNumber,secondNumber, total;
+
+
+        firstWord = "dog";
+        secondWord = "house";
+        firstNumber = 5;
+        secondNumber = 10;
+        total = firstNumber+secondNumber;
+
+        System.out.println(total);
+
+        /*.out.println(firstWord+secondWord);
+        System.out.println("the first number plus the second number is "+ (firstNumber+secondNumber));
+        System.out.println("the second number minus the first number is "+ (secondNumber-firstNumber));
+        System.out.println("the second number divided the first number is "+ (secondNumber/firstNumber));
+        System.out.println("the first number times the second number is "+ (firstNumber*secondNumber));
+        System.out.println("the modulus of the second number by the first number is "+ (secondNumber%firstNumber));*/
+
+
+    }
+
+}
